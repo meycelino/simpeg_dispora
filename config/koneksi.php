50128ae6358c41fa8b335273b45f15d4790d71d6
meycelino <meycelino@ymail.com>
2021-06-04 16:24:53 +0700

    koneksi hosting

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,9 +1,21 @@
 <?php
+	//localhost
+	/* 
 	$Open = mysql_connect("localhost","root","");
 		if (!$Open){
 		die ("Koneksi ke Engine MySQL Gagal !<br>");
 		}
 	$Koneksi = mysql_select_db("simpeg_dispora");
+		if (!$Koneksi){
+		die ("Koneksi ke Database Gagal !");
+		}
+	*/	
+	//server
+	$Open = mysql_connect("mahadewa","math3328","ayamgoreng2790");
+		if (!$Open){
+		die ("Koneksi ke Engine MySQL Gagal !<br>");
+		}
+	$Koneksi = mysql_select_db("math3328_simpeg_dispora");
 		if (!$Koneksi){
 		die ("Koneksi ke Database Gagal !");
 		}
